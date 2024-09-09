7edc8f79d3ae6528c5842611f117ad2bbe12468b
Michał <mkazimierczak2002@gmail.com>
2024-09-10 01:42:20 +0200

    Products getters and setters

--- src/main/java/com/infstos/backend/Product.java
+++ src/main/java/com/infstos/backend/Product.java
@@ -4,11 +4,84 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "products")
+
+@jakarta.persistence.Entity
 public class Product {
+    
     @Id
     private String id;
     private String name;
-    private double price;
+    private double baseprice;
+    private boolean isCable;
+    private String size;
+    private String color;
+    private String variant;
+    private String imageURL;
 
-    // Gettery i settery
-}
+        // Gettery i settery
+    
+        public String getId() {
+            return id;
+        }
+    
+        public void setId(String id) {
+            this.id = id;
+        }
+    
+        public String getName() {
+            return name;
+        }
+    
+        public void setName(String name) {
+            this.name = name;
+        }
+    
+        public double getBaseprice() {
+            return baseprice;
+        }
+    
+        public void setBaseprice(double baseprice) {
+            this.baseprice = baseprice;
+        }
+    
+        public boolean isCable() {
+            return isCable;
+        }
+    
+        public void setCable(boolean cable) {
+            this.isCable = cable;
+        }
+    
+        public String getSize() {
+            return size;
+        }
+    
+        public void setSize(String size) {
+            this.size = size;
+        }
+    
+        public String getColor() {
+            return color;
+        }
+    
+        public void setColor(String color) {
+            this.color = color;
+        }
+    
+        public String getVariant() {
+            return variant;
+        }
+    
+        public void setVariant(String variant) {
+            this.variant = variant;
+        }
+    
+        public String getImageURL() {
+            return imageURL;
+        }
+    
+        public void setImageURL(String imageURL) {
+            this.imageURL = imageURL;
+        }
+    }
+
